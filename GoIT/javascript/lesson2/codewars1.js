dc5723ec3940ad42368175ef9a24721b4bd275ed
OlyaMamontova <mamontolya@ukr.net>
2015-10-05 12:23:59 +0300

    homework2

---
+++ GoIT/javascript/lesson2/codewars1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Olya on 05.10.2015.
+ */
+function numberToString(num) {
+    // Return a string of the number here!
+    return num + '';
+}
---
+++ GoIT/javascript/lesson2/codewars2.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Olya on 05.10.2015.
+ */
+var stringToNumber = function(str){
+    return +str;
+}
