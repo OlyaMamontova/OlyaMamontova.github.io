f53ab6756ec03af9c4a1366d63cfd891aeb79555
OlyaMamontova <mamontolya@ukr.net>
2015-11-21 23:22:34 +0200

    homework15

--- GoIT/javascript/lesson15/cw7q4_array-dot-prototype-dot-reverse.js
+++ GoIT/javascript/lesson15/cw7q4_array-dot-prototype-dot-reverse.js
@@ -1,7 +1,21 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Array.prototype.reverse()
+The Array's reverse() method has gone missing! Re-write it, quick-sharp!
+When this method is called, it reverses the order of the items in the original 
+array. Then then it returns that same, original array. No new arrays should 
+need to be created to pass this kata.
  */
 
+Array.prototype.reverse = function() {
 
+  for(var i = 0, j = this.length-1; i < j; i++, j--){
+    var tmp = this[i];
+    this[i] = this[j];
+    this[j] = tmp;
+  }
+  return this;
+};
+
+var input = [1, 2, 3, 4];
+console.log(input.reverse()); // == [4, 3, 2, 1]  // returned by .reverse()
+console.log(input);           // == [4, 3, 2, 1]  // items reordered in the original array
