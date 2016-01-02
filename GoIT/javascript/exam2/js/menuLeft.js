bf2467a43f92f54d2b10706ea95e01246d1bb4d5
OlyaMamontova <mamontolya@ukr.net>
2016-01-03 01:21:01 +0200

    -

--- GoIT/javascript/exam2/js/menuLeft.js
+++ GoIT/javascript/exam2/js/menuLeft.js
@@ -1,8 +1,39 @@
+// background
+var total_pics_num = 4; // кількість зображень
+var interval = 6000;    // затримка між зображеннями
+var time_out = 0.5;     // затримка зміни зображень
+var i = 0;
+var timeout;
+var opacity = 100;
+function fade_to_next() {
+    opacity--;
+    var k = i + 1;
+    var image_now = 'bg_' + i;
+    if (i == total_pics_num) k = 1;
+    var image_next = 'bg_' + k;
+    document.getElementById(image_now).style.opacity = opacity/100;
+    document.getElementById(image_now).style.filter = 'alpha(opacity='+ opacity +')';
+    document.getElementById(image_next).style.opacity = (100-opacity)/100;
+    document.getElementById(image_next).style.filter = 'alpha(opacity='+ (100-opacity) +')';
+    timeout = setTimeout("fade_to_next()",time_out);
+    if (opacity==1) {
+        opacity = 100;
+        clearTimeout(timeout);
+    }
+}
+setTimeout (function go() {
+    i++;
+    if (i > total_pics_num) i=1;
+    fade_to_next();
+    setTimeout(go, interval);
+}, interval);
 
+
+
+//click on left menu
 var menuWrapper = document.querySelector('.menu_wrapper');
 var toggleMenu = document.querySelector('.toggle_menu');
 var home = menuWrapper.querySelector('.home');
-//var colRight = document.querySelector('.col_right');
 
 home.addEventListener('click', openOrCloseMenu);
 toggleMenu.addEventListener('click', openOrCloseMenu);
--- GoIT/javascript/exam2/js/tabs.js
+++ GoIT/javascript/exam2/js/tabs.js
@@ -1,31 +1,28 @@
-function changeBg(){
-    var body = document.getElementsByTagName('body')[0];
-    var i = 1;
 
-    setInterval(function go(){
-        var classBg = body.className.split(' ');
-        var currentClass = classBg[0].slice(0, -1) + i;
-        if(i === 4) i = 0;
-        body.classList.remove(classBg[0]);
-        i++;
-        body.classList.add(currentClass);
-    },5000);
 
-}
-
-changeBg();
-
+//right column
 $(document).ready(function () {
 
-    var tabContainers = $('.menu_item'); // получаем массив контейнеров
+    var tabContainers = $('.menu_item'); // РїРѕР»СѓС‡Р°РµРј РјР°СЃСЃРёРІ РєРѕРЅС‚РµР№РЅРµСЂРѕРІ
     $('ul.menu a').click(function () {
-        tabContainers.hide(); // прячем все табы
-        tabContainers.filter(this.hash).show(); // показываем содержимое текущего
+        tabContainers.hide(); // РїСЂСЏС‡РµРј РІСЃРµ С‚Р°Р±С‹
+        tabContainers.filter(this.hash).show(); // РїРѕРєР°Р·С‹РІР°РµРј СЃРѕРґРµСЂР¶РёРјРѕРµ С‚РµРєСѓС‰РµРіРѕ
         return false;
     }).filter(':first').click();
 });
 
+//switch photo about me
+$(document).ready(function(){
+    var photos = $('.menu1_about_me_photo_wrap');
+    photos.hide().filter(':first').show();
+    $('.switch_photo').click(function(){
+        photos.hide();
+        photos.filter(this.hash).show();
+        return false;
+    });
+});
 
+//show enlarged images
 $(document).ready(function(){
     var photos = $('.enlarged_img');
     $('.menu3_zoom').click(function(){
@@ -36,18 +33,10 @@ $(document).ready(function(){
     });
 });
 
+//hide enlarged images
 $(document).ready(function(){
     $('.close_button').click(function(){
         $('.enlarged_images').hide();
     });
 });
 
-$(document).ready(function(){
-    var photos = $('.menu1_about_me_photo_wrap');
-    photos.hide().filter(':first').show();
-    $('.switch_photo').click(function(){
-        photos.hide();
-        photos.filter(this.hash).show();
-        return false;
-    });
-});
